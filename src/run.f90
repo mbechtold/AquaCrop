ab8457da015c5163b96294b52ffa3d656b9892ab
Michel Bechtold <michel.bechtold@kuleuven.be>
2026-01-24 00:24:09 +0100

    ENH: Ageing adjustment

--- src/run.f90
+++ src/run.f90
@@ -3945,7 +3945,7 @@ subroutine RelationshipsForFertilityAndSaltStress()
                                   GetCrop_CCo(), GetCrop_CCx(), &
                                   GetCrop_CGC(), GetCrop_GDDCGC(), &
                                   GetCrop_CDC(), GetCrop_GDDCDC(), &
-                                  GetCrop_KcTop(), GetCrop_KcDecline(), &
+                                  GetCrop_KcTop(), GetCrop_KcDeclineCumul(), &
                                   real(GetCrop_CCEffectEvapLate(), kind= dp), &
                                   GetCrop_Tbase(), &
                                   GetCrop_Tupper(), GetSimulParam_Tmin(), &
@@ -4014,7 +4014,7 @@ subroutine RelationshipsForFertilityAndSaltStress()
                                   GetCrop_CCo(), GetCrop_CCx(), &
                                   GetCrop_CGC(), GetCrop_GDDCGC(), &
                                   GetCrop_CDC(), GetCrop_GDDCDC(), &
-                                  GetCrop_KcTop(), GetCrop_KcDecline(), &
+                                  GetCrop_KcTop(), GetCrop_KcDeclineCumul(), &
                                   real(GetCrop_CCEffectEvapLate(), kind=dp),  &
                                   GetCrop_Tbase(), GetCrop_Tupper(), &
                                   GetSimulParam_Tmin(), GetSimulParam_Tmax(), &
@@ -4894,7 +4894,7 @@ subroutine InitializeSimulationRunPart1()
                 GetCrop_GDDaysToFullCanopy(), GetCrop_GDDaysToSenescence(), &
                 GetCrop_GDDaysToHarvest(), GetCrop_CCo(), GetCrop_CCx(), &
                 GetCrop_CGC(), GetCrop_GDDCGC(), GetCrop_CDC(), GetCrop_GDDCDC(), &
-                GetCrop_KcTop(), GetCrop_KcDecline(), real(GetCrop_CCEffectEvapLate(),kind=dp), &
+                GetCrop_KcTop(), GetCrop_KcDeclineCumul(), real(GetCrop_CCEffectEvapLate(),kind=dp), &
                 GetCrop_Tbase(), GetCrop_Tupper(), GetSimulParam_Tmin(), &
                 GetSimulParam_Tmax(), GetCrop_GDtranspLow(), GetCO2i(), &
                 GetCrop_ModeCycle(), .true.))
